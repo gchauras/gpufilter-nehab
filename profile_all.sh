b5aa3b86b0de59810f494d86cff8e8da17ed03da
Gaurav Chaurasia <gchauras@mit.edu>
2015-01-13 17:40:19 -0500

    Minor change

--- profile_all.sh
+++ profile_all.sh
@@ -2,5 +2,5 @@ bin/box_filter 0 1 2> ~/Projects/recfilter-paper/perflogs/box_filter_1.nehab.per
 bin/box_filter 0 3 2> ~/Projects/recfilter-paper/perflogs/box_filter_3.nehab.perflogs
 bin/box_filter 0 6 2> ~/Projects/recfilter-paper/perflogs/box_filter_6.nehab.perflogs
 bin/summed_table 0 2> ~/Projects/recfilter-paper/perflogs/summed_table.nehab.perflogs
-# bin/bspline_filter  0 2> ~/Projects/recfilter-paper/perflogs/bspline_filter.nehab.perflogs
-# bin/gaussian_filter 0 2> ~/Projects/recfilter-paper/perflogs/gaussian_filter.nehab.perflogs
+bin/bspline_filter  0 2> ~/Projects/recfilter-paper/perflogs/bspline_filter.nehab.perflogs
+bin/gaussian_filter 0 2> ~/Projects/recfilter-paper/perflogs/gaussian_filter.nehab.perflogs
